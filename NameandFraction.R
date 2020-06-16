22495c7731fb2c356d5f37bfd067e37199a0140a
Aliana Britson <Britson.Aliana@deq.state.or.us>
2020-06-16 14:09:15 -0700

    added name and fraction function to package so it didn't have to call another package

---
+++ NameandFraction.R
@@ -0,0 +1,13 @@
+#function to add sample fraction to metals for NPDES RPA and Split sample purposes
+
+namefrac<-function(x)
+{x$Char_Name<-
+  ifelse(x$Char_Name %in% c("Calcium","Copper","Magnesium","Potassium","Sodium","Cyanide","Aluminum","Iron","Lead",
+                            "Mercury","Nickel","Silver","Thallium","Antimony","Arsenic","Beryllium","Cadmium","Chromium",
+                            "Zinc","Selenium","Chromium(III)","Chromium(VI)","Arsenic ion (3+)","Methylmercury(1+)","Organic carbon",
+                            "Barium","Cobalt","Manganese","Vanadium","Molybdenum","Boron","Lithium","Uranium","Hardness, Ca, Mg","Silica", 
+                            "Ammonia","Arsenic, Inorganic","Silicon") & !is.na(x$Sample_Fraction),
+         paste0(x$Char_Name,", ",x$Sample_Fraction),
+         x$Char_Name)
+  return(x)
+}
