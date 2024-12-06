5b17260f2c67695a4afda743fcac1b2faa0ebc4e
BROWN Daniel <Daniel.T.BROWN@deq.oregon.gov>
2024-12-06 13:40:59 -0800

    The Troubleshooter can be used to find issues in datasets by mimicing the Shiny app's steps outside of the app itself. This version will need some additional lines of code and better guidance before it's fully useable.

---
+++ Troubleshooter.R
@@ -0,0 +1,161 @@
+library(tidyverse)
+library(AWQMSdata)
+
+# Load functions
+source("C:/Users/dbrown/Split_Code/ParamGrp_Function.R")
+source("C:/Users/dbrown/Split_Code/QC_Issue.R")
+source("C:/Users/dbrown/Split_Code/Split_Comp_Function.R")
+source("C:/Users/dbrown/Split_Code/NameandFraction.R")
+
+# Enter parameters for data pull
+StDate <- '2023-07-05'
+EndDate <- '2023-07-07'
+Project <- 'Effluent Monitoring'
+SplOrg <- 'CITY_RAINIER(NOSTORETID)'
+
+# Pull data from AWQMS
+All_Data <- AWQMS_Data(startdate = StDate, enddate = EndDate, OrganizationID = c(SplOrg, 'OREGONDEQ'),
+                       project = Project, filterQC = FALSE) 
+
+# Create tables of DEQ and Split data (effectively lines 169-175 and 184-189 in app.R)
+deq <- subset(All_Data, OrganizationID=='OREGONDEQ') %>%
+  select(OrganizationID,Project1,MLocID,StationDes,Lat_DD,Long_DD,act_id,Activity_Type,SampleStartDate,SampleStartTime,SampleStartTZ,
+         SampleMedia,SampleSubmedia,Char_Name,Char_Speciation,Sample_Fraction,CASNumber,Result_status,Result_Type,
+         Result_Text,Result_Numeric,Result_Operator,Result_Unit,Method_Code,Method_Context,Analytical_Lab,Activity_Comment,
+         Result_Comment,QualifierAbbr,MDLType,MDLValue,MDLUnit,MRLType,MRLValue,MRLUnit)
+
+splt <- subset(All_Data, OrganizationID==SplOrg) %>%
+  select(OrganizationID,Project1,MLocID,StationDes,Lat_DD,Long_DD,act_id,Activity_Type,SampleStartDate,SampleStartTime,SampleStartTZ,
+         SampleMedia,SampleSubmedia,Char_Name,Char_Speciation,Sample_Fraction,CASNumber,Result_status,Result_Type,
+         Result_Text,Result_Numeric,Result_Operator,Result_Unit,Method_Code,Method_Context,Analytical_Lab,Activity_Comment,
+         Result_Comment,QualifierAbbr,MDLType,MDLValue,MDLUnit,MRLType,MRLValue,MRLUnit)
+
+# Step through the Split_Comp_Function.R code
+#####UNITS
+#convert units so they match DEQ's data (NOTE: THIS NEEDS MORE TESTING TO MAKE SURE THE CODE WORKS PROPERLY)
+#get the units associated with the characteristics for deq and split data
+dequnit<-subset(deq,select=c("Char_Name","Result_Unit"))
+dequnit<-unique(dequnit)
+spltunit<-subset(splt,select=c("Char_Name","Result_Unit"))
+spltunit<-unique(spltunit)
+
+#join deq units and split units together, only keep characteristics where units do not agree
+unjn<-inner_join(dequnit,spltunit,by=c("Char_Name"),suffix=c(".deq",".splt"))
+unjn<-subset(unjn, unjn$Result_Unit.splt!=unjn$Result_Unit.deq)
+
+#get characteristics where we need to convert from mg to ug and also from ug to mg
+#add ng/L as well
+#(the most likely units conversions needed, will add more as necessary)
+mgug<-subset(unjn, unjn$Result_Unit.splt=="mg/l" & unjn$Result_Unit.deq=="ug/l")
+ugmg<-subset(unjn, unjn$Result_Unit.splt=="ug/l" & unjn$Result_Unit.deq=="mg/l")
+ngug<-subset(unjn, unjn$Result_Unit.splt=="ng/l" & unjn$Result_Unit.deq=="ug/l")
+ugng<-subset(unjn, unjn$Result_Unit.splt=="ug/l" & unjn$Result_Unit.deq=="ng/l")
+
+#if there are any rows in mgug or ugmg then run data through unit conversion function
+#add ngug and ugng
+
+if (nrow(mgug)!=0) {splt<-unit_conv(splt,mgug$Char_Name,"mg/l","ug/l")}
+if (nrow(ugmg)!=0) {splt<-unit_conv(splt,ugmg$Char_Name,"ug/l","mg/l")}
+if (nrow(ngug)!=0) {splt<-unit_conv(splt,ngug$Char_Name,"ng/l","ug/l")} # changed column to ngug$Char_Name from ugmg$Char_Name - DTB 112024
+if (nrow(ugng)!=0) {splt<-unit_conv(splt,ugng$Char_Name,"ug/l","ng/l")} # changed column to ugng$Char_Name from ugmg$Char_Name - DTB 112024
+
+###JOIN DEQ AND SPLIT DATA
+#use namfrac function to get fraction as part of name for Metals 
+#(if we just use sample fraction as part of the join we might miss some characteristics that were uploaded with different sample fractions
+#where sample fraction doesn't matter (e.g. Dichlorodifluoromethane has been uploaded in AWQMS under Volatile and Extractable sample fractions))
+deq<-namefrac(deq)
+splt<-namefrac(splt)
+
+#If split org did nitrate analysis instead of nitrate-nitrite, and DEQ did nitrate-nitrate, convert split char name to nitrate-nitrite
+#so the two can be compared. Often the results are so similar due to nitrite easily converting to nitrate that the two are comparable. 
+
+if(any(deq$Char_Name %in% "Nitrate + Nitrite")) {splt<-splt%>% mutate(Char_Name=str_replace(Char_Name,"Nitrate","Nitrate + Nitrite"))}
+
+#need to join datasets on an inner join
+jn<-inner_join(deq,splt, by = c('MLocID',"SampleStartDate","Char_Name","Activity_Type"),suffix=c(".deq",".split"))
+
+
+
+#need to add a column to determine what type of QC is needed (eg. RPA, Diff, Microdiff)
+#difference is done when data is less than 5x MRL. don't always get MRL from split lab, use ours to be safe
+jn$qctype<-case_when(jn$Char_Name=="Enterococcus"|jn$Char_Name=="Escherichia coli"|jn$Char_Name=="Fecal Coliform"|jn$Char_Name=="Total Coliform"~"Micro",
+                     jn$Char_Name==c("pH")|jn$Result_Numeric.deq<=(5*jn$MRLValue.deq)|jn$Result_Numeric.split<=(5*jn$MRLValue.deq)~"Diff",
+                     jn$Result_Numeric.deq>(5*jn$MRLValue.deq)& jn$Result_Numeric.split>(5*jn$MRLValue.deq) ~"RPD"
+)
+
+
+#calculate RPD
+jn$splitRPD<- case_when(jn$qctype=="RPD"~ 
+                          (abs(jn$Result_Numeric.deq-jn$Result_Numeric.split)/((jn$Result_Numeric.deq+jn$Result_Numeric.split)/2)*100)
+)
+
+#Calculate difference for Diff and Micro don't always get MRL from split lab, use ours to be safe 
+#if micro result is 0, use DEQ MRL since log of 0 is infinity. 
+jn$splitDiff<- case_when(jn$qctype=="Diff" & jn$Result_Operator.deq=="=" & jn$Result_Operator.split=="=" 
+                         ~abs(jn$Result_Numeric.deq-jn$Result_Numeric.split),
+                         jn$qctype=="Diff" & jn$Result_Operator.deq!="=" & jn$Result_Operator.split=="=" 
+                         ~abs(jn$Result_Numeric.split-jn$MRLValue.deq),
+                         jn$qctype=="Diff" & jn$Result_Operator.deq=="=" & jn$Result_Operator.split!="=" 
+                         ~abs(jn$Result_Numeric.deq-jn$MRLValue.split), 
+                         jn$qctype=="Micro" & (jn$Result_Operator.deq=="="|jn$Result_Operator.deq==">") & 
+                           (jn$Result_Operator.split=="="|jn$Result_Operator.split==">") &
+                           jn$Result_Numeric.deq!=0 & jn$Result_Numeric.split!=0
+                         ~abs(log10(jn$Result_Numeric.deq)-log10(jn$Result_Numeric.split)),
+                         jn$qctype=="Micro"& (jn$Result_Operator.deq=="="|jn$Result_Operator.deq==">") & 
+                           (jn$Result_Operator.split!="="|jn$Result_Operator.split!=">") &
+                           jn$Result_Numeric.deq!=0 & jn$Result_Numeric.split!=0
+                         ~abs(log10(jn$Result_Numeric.deq)-log10(jn$MRLValue.deq)),
+                         jn$qctype=="Micro"& (jn$Result_Operator.deq!="="|jn$Result_Operator.deq!=">") & 
+                           (jn$Result_Operator.split=="="|jn$Result_Operator.split==">") &
+                           jn$Result_Numeric.deq!=0 & jn$Result_Numeric.split!=0
+                         ~abs(log10(jn$Result_Numeric.split)-log10(jn$MRLValue.deq)),
+                         jn$qctype=="Micro" &  (jn$Result_Operator.split=="="|jn$Result_Operator.split==">") & jn$Result_Numeric.deq==0
+                         ~abs(log10(jn$MRLValue.deq)-log10(jn$Result_Numeric.split)),
+                         jn$qctype=="Micro" & (jn$Result_Operator.deq=="="|jn$Result_Operator.deq==">") & jn$Result_Numeric.split==0
+                         ~abs(log10(jn$MRLValue.deq)-log10(jn$Result_Numeric.deq))
+)
+
+#round RPD and Diff
+jn$splitDiff<-round(jn$splitDiff,digits=2)
+jn$splitRPD<-round(jn$splitRPD,digits=2)
+
+
+#need to remove samples from join where the split was taken too far apart in time 
+#(30 min for now- ask, may need to be taken down to 15-20)
+jn$timediff.deq<-as.difftime(jn$SampleStartTime.deq,units="mins")
+jn$timediff.split<-as.difftime(jn$SampleStartTime.split,units="mins")
+jn$timediff<-abs(jn$timediff.deq-jn$timediff.split)
+jn<-subset(jn, jn$timediff<=30)
+
+#need to return table of important columns
+#won't include lab comments- they are generic language set by AWQMS and not very helpful
+jn<-subset(jn,select=c("MLocID","Activity_Type","SampleStartDate","SampleStartTime.deq","Char_Name","StationDes.deq",
+                       "Char_Speciation.deq","Result_status.deq","Result_status.split",
+                       "Result_Type.deq","Result_Type.split","Result_Text.deq","Result_Text.split",
+                       "Result_Unit.deq","Result_Unit.split","Method_Code.deq","Method_Code.split",
+                       "Activity_Comment.deq","Result_Comment.deq","Activity_Comment.split","Result_Comment.split",
+                       "MRLType.deq","MRLValue.deq","MRLUnit.deq","MRLType.split","MRLValue.split","MRLUnit.split",
+                       "qctype","splitRPD","splitDiff"
+))
+
+# Change dataframe name to match SplitReport_RMarkdown.Rmd
+spltcomp <- jn
+
+#add issue column 
+spltcomp<-qc_issue(spltcomp)
+
+#add j flag to data below MRL for split data reported to MDL
+spltcomp$Result_Text.split<-ifelse(!is.na(spltcomp$MRLValue.split) & substr(spltcomp$Result_Text.split,start=1,stop=1)!="<" & (as.numeric(spltcomp$Result_Text.split)<=spltcomp$MRLValue.split),paste0(spltcomp$Result_Text.split," J"),spltcomp$Result_Text.split)
+
+#make summary tables conditional, if WWTP split, just have the one table. If it is a landfill split, break it out into groups
+
+if(unique(spltorg$Project1)=="Effluent Monitoring") {
+  iss<-subset(spltcomp,issue=="TRUE",select=c(MLocID,Char_Name,Result_Text.deq,Result_Text.split,
+                                              Result_Unit.deq,splitRPD,splitDiff))
+  
+  #change name of "Biochemical oxygen demand, standard conditions" to "BOD-5" (need to do after param_grp function for names' sake)
+  iss<-iss%>%
+    mutate(Char_Name=str_replace(Char_Name,"Biochemical oxygen demand, standard conditions","BOD-5"))%>%
+    mutate(Char_Name=str_replace(Char_Name,"Total suspended solids","TSS"))%>%
+    mutate(Char_Name=str_replace(Char_Name,"Chemical oxygen demand","COD"))%>%
+    mutate(Char_Name=str_replace(Char_Name,"Hardness, Ca, Mg, Total Recoverable","Hardness"))
