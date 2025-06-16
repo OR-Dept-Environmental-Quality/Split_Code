d09da77c92c5c553e568cb6cd2ce6f0c4fa6cf78
THOMAS Carter <Carter.Thomas@deq.oregon.gov>
2025-06-16 12:37:24 -0700

    adding some lines to set working directory so file pathing is automatic

--- AWQMS_credentials.R
+++ AWQMS_credentials.R
@@ -2,3 +2,4 @@ AWQMSdata::AWQMS_credentials(AWQMS_usr  = "oregondeq",
                              AWQMS_pass = "Chinook1859",
                              overwrite = TRUE,
                              install = TRUE)
+
