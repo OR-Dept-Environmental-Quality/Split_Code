86bb5e970c53e4a2b8e22669abb327eba45c279f
THOMAS Carter <Carter.Thomas@deq.oregon.gov>
2026-07-30 10:54:50 -0700

    edits

--- AWQMS_credentials.R
+++
@@ -1,5 +0,0 @@
-AWQMSdata::AWQMS_credentials(AWQMS_usr  = "oregondeq",
-                             AWQMS_pass = "Chinook1859",
-                             overwrite = TRUE,
-                             install = TRUE)
-
